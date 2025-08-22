4cb5a528558f19b1550ddd663885e0b4ec27eb68
mkharound <mkh@a-round.dk>
2025-08-22 10:30:07 +0200

    feat: migrations

---
+++ database/migrations/2025_08_14_100001_create_order_lines_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_lines', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->integer('quantity')->default(1);
+
+            $table->decimal('price', 10, 2);
+
+            $table->timestamps();
+
+            $table->foreignIdFor(\App\Models\Order::class)
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+
+            $table->foreignIdFor(\App\Models\Product::class)
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_lines');
+    }
+};
