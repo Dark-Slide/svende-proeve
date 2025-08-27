55e8eec69b1cffe08cd845018c7d0bba2c4482a6
mkharound <mkh@a-round.dk>
2025-08-27 10:50:29 +0200

    feat: moved api routes out of web.php

---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\GetController;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\Rules\Password;
+
+Route::get('/sanctum/csrf-cookie', fn () => response()->noContent()); // Sanctum handles cookie
+
+// Frontpage
+Route::get('/api/frontpage', [GetController::class, 'frontpage'])
+    ->withoutMiddleware('Tymon\JWTAuth\Http\Middleware\Authenticate');
+
+
+// Products
+Route::get('/api/products', [GetController::class, 'products'])
+    ->withoutMiddleware('Tymon\JWTAuth\Http\Middleware\Authenticate');
+
+Route::get('/api/product/{id}', [GetController::class, 'product'])
+    ->withoutMiddleware('Tymon\JWTAuth\Http\Middleware\Authenticate');
+
+Route::post('/api/product/create', [PostController::class, 'create_product'])
+    ->withoutMiddleware('Illuminate\Foundation\Http\Middleware\VerifyCsrfToken');
+
+// User
+
+Route::get('/api/user/{id}', [GetController::class, 'user'])
+    ->withoutMiddleware('Tymon\JWTAuth\Http\Middleware\Authenticate');
+
+
+Route::get('/{any}', function () {
+    return file_get_contents(public_path('frontend/dist/frontend/index.html'));
+})->where('any', '^(?!api).*$')->withoutMiddleware('Tymon\JWTAuth\Http\Middleware\Authenticate');
