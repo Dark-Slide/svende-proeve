469f229c0f55de54ace03afa9c223ecbb900a75f
Kondibeer <kenneth@lendvald.dk>
2025-09-06 21:37:34 +0200

    maxfix

--- frontend/src/app/components/product-pages/product/products/products.component.ts
+++ frontend/src/app/components/product-pages/product/products/products.component.ts
@@ -13,13 +13,10 @@ import { Colours } from 'src/app/models/colours';
 import { Materials } from 'src/app/models/materials';
 import { Types } from 'src/app/models/types';
 import { Conditions } from 'src/app/models/conditions';
-
-
-
-
-
-
-
+import { ColourService } from 'src/app/Services/colour.service';
+import { MaterialService } from 'src/app/Services/material.service';
+import { TypeService } from 'src/app/Services/type.service';
+import { ConditionsService } from 'src/app/Services/condition.service'; 
 
 
 
@@ -35,7 +32,15 @@ import { Conditions } from 'src/app/models/conditions';
 
 export class ProductsComponent implements OnInit {
 
-  constructor(private productService: ProductService, private categoryService: CategoryService) {}
+  constructor(
+    private productService: ProductService,
+    private categoryService: CategoryService,
+    private materialService: MaterialService,
+    private colourService: ColourService,
+    private typeService: TypeService,
+    private conditionService: ConditionsService,
+    private router: Router
+  ) {}
   
   public SortOrder = SortOrder; 
   
@@ -54,6 +59,11 @@ export class ProductsComponent implements OnInit {
   ngOnInit() {
     this.productService.getProducts().subscribe(x => this.products = x);
     this.categoryService.getAllCategories().subscribe(x => this.categories = x);
+    this.materialService.getAllMaterials().subscribe(x => this.materials = x);
+    this.colourService.getAllColours().subscribe(x => this.colours = x);
+    this.typeService.getAllTypes().subscribe(x => this.types = x);
+    this.conditionService.getAllConditions().subscribe(x => this.conditions = x);
+    this.filteredProducts = this.products;
   }
 
   searchFilteredProducts(){
@@ -71,6 +81,12 @@ export class ProductsComponent implements OnInit {
 
   }
 
+
+  getColourName(colourId: string | undefined): string {
+    const colour = this.colours.find(c => c.id === Number(colourId));
+    return colour ? colour.name : 'Ukendt farve';
+  }
+
   sortTheProducts() {
     switch (this.sortSelected) {
       case SortOrder.PriceAsc:
@@ -80,7 +96,7 @@ export class ProductsComponent implements OnInit {
         this.products.sort((a, b) => b.price - a.price);
         break;
       case SortOrder.ColourAsc:
-        this.products.sort((a, b) => a.colour.localeCompare(b.colour));
+        this.products.sort((a, b) => this.getColourName(a.colour).localeCompare(this.getColourName(b.colour)));
         break;
       case SortOrder.ColourDesc:
         this.products.sort((a, b) => b.colour.localeCompare(a.colour));
@@ -112,6 +128,32 @@ export class ProductsComponent implements OnInit {
       default:
         break;
   }
+  
+}
+selectedMaterialId: number | null = null;
+selectedColourId: number | null = null;
+selectedTypeId: number | null = null;
+selectedConditionId: number | null = null;
+filterProducts() {
+  let filtered = [...this.products];
+
+  if (this.selectedMaterialId) {
+    filtered = filtered.filter(product => String(product.material) === String(this.selectedMaterialId));
+  }
+
+  if (this.selectedColourId) {
+    filtered = filtered.filter(product => String(product.colour) === String(this.selectedColourId));
+  }
+
+  if (this.selectedTypeId) {
+    filtered = filtered.filter(product => String(product.type) === String(this.selectedTypeId));
+  }
+
+  if (this.selectedConditionId) {
+    filtered = filtered.filter(product => String(product.condition) === String(this.selectedConditionId));
+  }
+
+  this.filteredProducts = filtered; 
 }
 
 
