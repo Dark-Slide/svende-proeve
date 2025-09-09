31fb2f0aa8db8de3267714784f8f0ab876925c71
mkharound <mkh@a-round.dk>
2025-09-09 15:34:37 +0200

    feat: media model

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -4,7 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Media
+ * @property int $id
+ * @property string $path
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ */
 class Media extends Model
 {
-    //
+    protected $fillable = ['filename', 'path', 'type', 'size'];
 }
