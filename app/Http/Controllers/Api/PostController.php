807b3c7ad680d9533aff99ef99ccc52e9bb80431
mkharound <mkh@a-round.dk>
2025-09-09 16:36:43 +0200

    fixed: naming of colors to colours

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -31,7 +31,7 @@ class PostController extends Controller
             ->first();
 
         $color = Color::query()
-            ->where('id', $data['colors'] ?? null)
+            ->where('id', $data['colours'] ?? null)
             ->select('id')
             ->first();
 
